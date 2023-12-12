a38e0f3a1751b5b6b433292864b00cd0129cdc38
thomas <inquister12345678@gmail.com>
2023-12-12 13:03:46 -0800

    adjusted some scripts and paths

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client')));
 }
 
 const server = new ApolloServer({
@@ -36,5 +36,6 @@ db.once('open', () => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
+
